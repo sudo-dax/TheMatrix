190f9873b2e84182e89d77bdc0a8054ab3328c73
sudo-dax <dax.scholtz@gmail.com>
2022-03-13 00:56:23 +1000

    Matrix Reloaded...

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-var symbol;
-var symbolSize = 18;
+var symbolSize = 13;
 var streams = [];
+var fadeInterval = 1.6;
 
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
+  createCanvas(
+      window.innerWidth, 
+      window.innerHeight
+      );
   background(0);
+
   var x = 0;
   for (var i = 0; i <= width / symbolSize; i++) {
     var stream = new Stream();
-    stream.generateSymbols(x, random(-1000, 0));
+    stream.generateSymbols(x, random(-2000, 0));
     streams.push(stream);
     x += symbolSize;
   }
+
+  textFont('Consolas')
   textSize(symbolSize);
 }
 
@@ -22,20 +28,29 @@ function draw() {
   });
 }
 
-function Symbol(x, y, speed, first) {
+function Symbol(x, y, speed, first, opacity) {
   this.x = x;
   this.y = y;
   this.value;
 
   // Change Speed of Character Fall
   this.speed = speed;
+  this.first = first;
   // Change Rate of Symbol Switching
   this.switchInterval = round(random(7, 28));
-  this.first = first;
+  this.opacity = opacity;
 
   this.setToRandomSymbol = function () {
+    var charType = round(random(0, 5));
     if (frameCount % this.switchInterval == 0) {
-      this.value = String.fromCharCode(0x30a0 + round(random(0, 96)));
+        if(charType > 1) {
+            // Set it to Katana
+            this.value = String.fromCharCode(
+                0x30a0 + round(random(0, 96)));
+        } else {
+            // set it to numeric
+            this.value = floor(random(0, 10));
+        }
     }
   };
 
@@ -51,11 +66,13 @@ function Stream() {
   this.speed = random(2, 6.2);
 
   this.generateSymbols = function (x, y) {
+    var opacity = 255;
     var first = round(random(0, 4)) == 1;
     for (var i = 0; i <= this.totalSymbols; i++) {
-      symbol = new Symbol(x, y, this.speed, first);
+      symbol = new Symbol(x, y, this.speed, first, opacity);
       symbol.setToRandomSymbol();
       this.symbols.push(symbol);
+      opacity -= (255 / this.totalSymbols) / fadeInterval;
       y -= symbolSize;
       first = false;
     }
@@ -64,9 +81,9 @@ function Stream() {
   this.render = function () {
     this.symbols.forEach(function (symbol) {
       if (symbol.first) {
-        fill(180, 255, 180);
+        fill(180, 255, 180, symbol.opacity);
       } else {
-        fill(0, 255, 70);
+        fill(0, 255, 70, symbol.opacity);
       }
       text(symbol.value, symbol.x, symbol.y);
       symbol.rain();
